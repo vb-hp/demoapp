cc311dc5cdde9909303906e6e39974a22371eaf1
Venkat Ballem <venkat.ballem@philips.com>
2020-05-10 19:30:22 +0530

    test back with master

--- code/SampleProgramToInterface/src/main/java/com/sample/bad/GodClassCaller.java
+++ code/SampleProgramToInterface/src/main/java/com/sample/bad/GodClassCaller.java
@@ -3,5 +3,7 @@ package com.sample.bad;
 public class GodClassCaller {
 
     public GodClassCaller() {
+        GodClass godClass = new GodClass();
+        System.out.println(godClass.isRooted());
     }
 }
