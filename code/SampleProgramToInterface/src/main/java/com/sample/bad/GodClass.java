58c44ccffe140e7ab0d7e221186c5d0b0b213d8e
Venkat Ballem <venkat.ballem@philips.com>
2020-05-10 18:40:49 +0530

    added some bad code to test

---
+++ code/SampleProgramToInterface/src/main/java/com/sample/bad/GodClass.java
@@ -0,0 +1,26 @@
+package com.sample.bad;
+
+public class GodClass {
+
+    public boolean isRooted() {
+        boolean a = true;
+        boolean b = true;
+        boolean c = true;
+        boolean d = true;
+        boolean e = true;
+
+        if (a == true) {
+            return true;
+        } else if (b == true) {
+            return true;
+        } else if (c == true) {
+            return true;
+        } else if (d == true) {
+            return true;
+        } else if (e == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}  
